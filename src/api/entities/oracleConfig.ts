7abe09e146b6e48832a0d591f21bf154d94aa6a4
oneke343 <oneke343@gmail.com>
2025-08-23 12:32:04 +0800

    fix fix

--- src/api/entities/oracleConfig.ts
+++ src/api/entities/oracleConfig.ts
@@ -264,7 +264,7 @@ export class OracleConfig {
     /**
      * Get the current configuration for the Chainlink Oracle.
      */
-
+    getChainlinkOracleConfig(): Maybe<ChainlinkOracleConfig> {
         if (!this.isChainlinkOracle) {
             return null;
         }
